e67d0689687689fcde11c568bf01df647b8eed2a
PaGenaro <80373174+PaGenaro@users.noreply.github.com>
2022-06-16 21:16:52 -0300

    "Ajuste sobre novas tentativas"

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Tela de Verificação de E-mail com CSS/ HTML /JavaScript
 
 # Function
 
-Apó digitar o email o input passará informação se o e-mail é valido ou não, através de cor e mensagem.
+Após digitar o email o input passará informação se o e-mail é valido ou não, através de cor e mensagem.
 
--- index.js
+++ index.js
@@ -1,31 +1,58 @@
+const emailsValidos = [];
+
 function validarEmail(){
-     var email = document.querySelector('#email');
-     var error = document.querySelector('#error-email');
+    var email = document.querySelector('#email');
+    var error = document.querySelector('#error-email');
 
-     if(!email.checkValidity())
-          { error.innerHTML = "Email inválido";}
+    if(!email.checkValidity())
+         { error.innerHTML = "Email inválido";}
          
-    }
+   else{return emailsValidos.push(email)}
 
-    function redefinirMsg(){
-        var error = document.querySelector('#error=email');
-        if (error.innerHTML == "Email inválido"){
-            error.innerHTML ="";
-        }
-    }
+   }
 
-    function adicionar(){
-        var email = document.querySelector('#email');
-        if (!email.checkValidity == true){
-            this.adicionar(email);
-        }
-    }
-  
+   console.log(emailsValidos)
+
+
+    /* Para validar email está ok*/
+
+   /*Agora tentativas para incluir na lista*/
+
+   let input =document.getElementById('#email');
+   let btnAdd =document.getElementById('#btn-add');
+   let table =document.getElementById('#areaLista');
+
+   function addTarefa(){
+    let valorInput = input.value;
+
+    if ((valorInput !== error) && (valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)){
+      let novoItem=`<table id="areaLista">
+        <thead>
+          <th class="funcaoemail">Emails Validos</th>
+          <th class="funcaoacao">Ação</th>
+      </thead>
+
+          <tr id="lista">
+            <td> ${valorInput}</td>
+
+            <td>x</td>
+          </tr>
+         <tbody id="tbody">
+            
+         </tbody>
+      </table>`;
+
+    table.innerHTML += novoItem;
+
+    input.value ="";
+    input.focus();
     
-    /* 
-     Aqui estava tentanto igual o video que vi mas não deu certo.
+    }
+   }
 
+  /* Aqui estava foi a primeira tentativa que não deu certo*/
 
+ /*
     class Email{
 
 
